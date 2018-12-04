a5949fdf376f1c4a4ec41dbd44996216c12527dd
jpvhcavalheiro <jpvhcavalheiro@gmail.com>
2018-12-04 20:14:25 +0000

    Initial commit

---
+++ StockManagementStates3FloorsEndpoints/src/main/java/io/altar/models/Entity.java
@@ -0,0 +1,14 @@
+package io.altar.models;
+
+import java.io.Serializable;
+
+public class Entity implements Serializable {
+	private static final long serialVersionUID=1L;
+private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+}
